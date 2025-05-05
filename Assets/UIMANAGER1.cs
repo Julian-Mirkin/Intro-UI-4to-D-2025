89736bc34f08ee51d7eb1c23c2e90da08163df8b
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-05-05 10:36:53 -0300

    terminao

---
+++ Assets/UIMANAGER1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIMANAGER1 : MonoBehaviour
+{
+
+    public TMP_InputField InputClave;
+    public TextMeshProUGUI txtResultado;
+    string claveIngresada;
+    string claveCorrecta = "jeroCrack";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = " ";
+    }
+
+    public void Clave()
+    {
+        claveIngresada = InputClave.text;
+
+        if (claveIngresada == claveCorrecta)
+        {
+            txtResultado.text = "Clave correcta";
+        }
+        else
+        {
+            txtResultado.text = "Clave incorrecta";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
